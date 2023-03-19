104a16dabc470f546a8438c421c0b2c083ec21a2
austinpkugler <austinpkugler@gmail.com>
2023-03-18 17:36:18 -0700

    Improve test data

--- create_db.py
+++ create_db.py
@@ -12,6 +12,9 @@ from app import app, db, models
 
 
 def create_db():
+    def random_binary_string(n):
+            return ''.join(str(random.randint(0, 1)) for _ in range(n))
+
     with app.app_context():
         db_path = os.path.join('instance', os.environ.get('DATABASE_URL').split('///')[1])
         if os.path.exists(db_path):
@@ -20,15 +23,15 @@ def create_db():
         db.create_all()
 
         # PlugConfig test data
-        for i in range(5):
+        for i in range(4, 8):
             plug = models.PlugConfig(
                 name='{}-Pin Plug'.format(i),
-                cure_profile='0100101101',
-                horizontal_offset=random.uniform(0.1, 5),
-                vertical_offset=random.uniform(0.1, 5),
-                horizontal_gap=random.uniform(0.1, 5),
-                vertical_gap=random.uniform(0.1, 5),
-                slot_gap=random.uniform(0.1, 5)
+                cure_profile=random_binary_string(i),
+                horizontal_offset=round(random.uniform(0.1, 5), 2),
+                vertical_offset=round(random.uniform(0.1, 5), 2),
+                horizontal_gap=round(random.uniform(0.1, 5), 2),
+                vertical_gap=round(random.uniform(0.1, 5), 2),
+                slot_gap=round(random.uniform(0.1, 5), 2)
             )
             db.session.add(plug)
         db.session.commit()
